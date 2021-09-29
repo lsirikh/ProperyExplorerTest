7fd0ff0f5b140a63392bbc7206a57f16ed24de0a
lsirikh <lsirikh@naver.com>
2021-09-29 12:42:35 +0900

    Add Comments

--- PropertyExplorerTest/Defines/Interfaces/IPropertyContainer.cs
+++ PropertyExplorerTest/Defines/Interfaces/IPropertyContainer.cs
@@ -8,12 +8,25 @@ namespace PropertyExplorerTest.Defines.Interfaces
 {
     public interface IPropertyContainer
     {
+        /// <summary>
+        /// PropertyContainer의 명칭 Color, Size 등등...
+        /// </summary>
         string PropertyName { get; }
 
+        /// <summary>
+        /// 소문자 형태 글자
+        /// </summary>
         string LowerPropertyName { get; }
 
+        /// <summary>
+        /// Show Hide 설정을 위한 boolean 필드
+        /// </summary>
         bool IsShow { get; set; }
 
+        /// <summary>
+        /// 실제 입력될 속정 값 Property
+        /// ex) 3(두께정보), 132(X)
+        /// </summary>
         IPropertySet Property { get; }
 
         IPropertyCategory ParentCategory { get; }
--- PropertyExplorerTest/Models/BaseModel.cs
+++ PropertyExplorerTest/Models/BaseModel.cs
@@ -8,34 +8,70 @@ using System.Windows.Media;
 namespace PropertyExplorerTest.Models
 {
     public class BaseModel
-    {
+    {   
+        /// <summary>
+        /// 넓이 Width
+        /// </summary>
         public double Width { get; set; } = 100;
 
+        /// <summary>
+        /// 높이 Height
+        /// </summary>
         public double Height { get; set; } = 100;
 
-        public double X { get; set; } 
+        /// <summary>
+        /// 좌표정보 X
+        /// Canvas에 붙일 때 사용된다.
+        /// </summary>
+        public double X { get; set; }
 
-        public double Y { get; set; } 
+        /// <summary>
+        /// 좌표정보 X
+        /// Canvas에 붙일 때 사용된다.
+        /// </summary>
+        public double Y { get; set; }
 
+        /// <summary>
+        /// FillColor 도형 내부의 컬러
+        /// </summary>
         public Color FillColor { get; set; } = Color.FromRgb(0xff, 0x00, 0x00);
 
+        /// <summary>
+        /// 도형 선의 색깔
+        /// </summary>
         public Color BorderColor { get; set; } = Color.FromRgb(0x24, 0x00, 0xff);
 
+        /// <summary>
+        /// 도형 선의 두께
+        /// </summary>
         public double BorderThickness { get; set; } = 1;
 
+        /// <summary>
+        /// 마우스 팝업 설명을 위한 Tooltip
+        /// </summary>
         public string ToolTip { get; set; }
 
         public BaseModel()
         {
 
         }
-
+        /// <summary>
+        /// 생성자 오버라이드 param 2개
+        /// </summary>
+        /// <param name="x">좌표정보 X</param>
+        /// <param name="y">좌표정보 Y</param>
         public BaseModel(double x, double y)
         {
             this.X = x;
             this.Y = y;
         }
-
+        /// <summary>
+        /// 생성자 오버라이드 param 4개
+        /// </summary>
+        /// <param name="w">넓이 Width</param>
+        /// <param name="h">높이 Height</param>
+        /// <param name="x">좌표정보 X</param>
+        /// <param name="y">좌표정보 Y</param>
         public BaseModel(double w, double h, double x, double y)
         {
             this.Width = w;
